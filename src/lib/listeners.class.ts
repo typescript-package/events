1c800ba8710ff7e2726acda6b959f0edafc06587
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-10-24 14:26:31 +0000

    refactor(`Listeners`): handle chaining, aggregate errors in emit, use properly pause, add listeners in the constructor.

--- src/lib/listeners.class.ts
+++ src/lib/listeners.class.ts
@@ -5,54 +5,61 @@
  * @template {(...args: any[]) => void} Listener 
  */
 export class Listeners<Listener extends (...args: any[]) => void> {
+  /**
+   * @description Privately stored listeners.
+   * @type {Set<Listener>}
+   */
   #listeners: Set<Listener> = new Set();
 
+  /**
+   * @description Privately stored state of pause.
+   * @type {boolean}
+   */
   #paused = false;
 
-  public pause() { this.#paused = true; }
-  public resume() { this.#paused = false; }
-
   /**
-   * @description Add a listener
-   * @public
-   * @param {Listener} listener 
+   * Creates an instance of `Listeners`.
+   * @constructor
+   * @param {...Listener[]} listeners The listeners to add.
    */
-  public add(listener: Listener) {
-    this.#listeners.add(listener);
+  constructor(...listeners: Listener[]) {
+    listeners?.forEach(listener => this.add(listener));
   }
 
   /**
-   * @description Add a listener that will be called only once
+   * @description Adds a listener.
    * @public
    * @param {Listener} listener 
    */
-  public once(listener: Listener) {
-    const wrapper = (...args: Parameters<Listener>) => {
-      this.delete(wrapper as Listener);
+  public add(listener: Listener, { once }: { once: boolean} = { once: false }): this {
+    const listenerOnce = ((...args: Parameters<Listener>) => {
+      this.delete(listenerOnce as Listener);
       listener(...args);
-    };
-    this.add(wrapper as Listener);
+    }) as Listener;
+    return this.#listeners.add(once ? listenerOnce : listener), this;
   }
 
   /**
-   * @description Remove a listener
+   * @description Removes a listener.
    * @public
    * @param {Listener} listener 
+   * @returns this
    */
-  public delete(listener: Listener) {
-    this.#listeners.delete(listener);
+  public delete(listener: Listener): this {
+    return this.#listeners.delete(listener), this;
   }
 
   /**
-   * @description Remove all listeners
+   * @description Removes all listeners.
    * @public
+   * @returns this
    */
-  public clear() {
-    this.#listeners.clear();
+  public clear(): this {
+    return this.#listeners.clear(), this;
   }
 
   /**
-   * @description Get the current number of listeners
+   * @description Gets the current number of listeners.
    * @public
    * @returns {number} 
    */
@@ -61,19 +68,24 @@ export class Listeners<Listener extends (...args: any[]) => void> {
   }
 
   /**
-   * @description Emit an event to all listeners
+   * @description Emits an event to all listeners.
    * @public
    * @param {...Parameters<Listener>} args 
    */
-  public emit(...args: Parameters<Listener>) {
+  public emit(...args: Parameters<Listener>): this {
+    if (this.#paused) return this;
+    const errors: any[] = [];
     for (const listener of this.#listeners) {
       try {
         listener(...args);
       } catch (error) {
-        // Optionally log or handle errors here
-        // console.error("Listener error:", error);
+        errors.push(error);
       }
     }
+    if (errors.length) {
+      throw new AggregateError(errors, "One or more listeners failed");
+    }
+    return this;
   }
 
   /**
@@ -84,13 +96,37 @@ export class Listeners<Listener extends (...args: any[]) => void> {
    * @returns {*} 
    */
   public async emitAsync(...args: Parameters<Listener>) {
+    if (this.#paused) return this;
     for (const listener of this.#listeners) {
-      await listener(...args);
+      await Promise.resolve(listener(...args));
     }
+    return this;
   }
 
   /**
-   * @description Return a snapshot of listeners (for debugging or introspection)
+   * @description Adds listener invoked once.
+   * @public
+   * @param {Listener} listener 
+   * @returns {this} 
+   */
+  public once(listener: Listener): this {
+    return this.add(listener, { once: true });
+  }
+
+  /**
+   * @description Pauses emitting.
+   * @public
+   */
+  public pause(): this { return this.#paused = true, this; }
+
+  /**
+   * @description Resume emitting.
+   * @public
+   */
+  public resume(): this { return this.#paused = false, this; }
+
+  /**
+   * @description Returns a snapshot of listeners.
    * @public
    * @returns {Listener[]} 
    */
